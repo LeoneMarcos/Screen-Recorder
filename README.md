eb9003eee127e9d55d496a88f15129f0d2cdf53b
LM <leone77marcos@gmail.com>
2023-08-22 02:54:56 -0300

    0.1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Screen Recorder
- Application developed in python with the premise of being a simple and functional screen recorder for desktops.
+ Website with the premise of being a simple and functional screen recorder for desktops.
---
+++ ScreenRecorder.js
@@ -0,0 +1,31 @@
+const recorderButton = document.getElementById("Recorder");
+let recorder;
+let chunks = [];
+let recording = false;
+
+recorderButton.addEventListener("click", async () => {
+    if (!recording) {
+        const stream = await navigator.mediaDevices.getDisplayMedia({
+            video: { mediaSource: "screen" }
+        });
+        recorder = new MediaRecorder(stream);
+        recorder.ondataavailable = (event) => chunks.push(event.data);
+        recorder.onstop = () => {
+            const blob = new Blob(chunks, { type: "video/webm" });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = "recording.webm";
+            a.click();
+            URL.revokeObjectURL(url);
+            chunks = [];
+            recorderButton.textContent = "REC";
+            recording = false;
+        };
+        recorder.start();
+        recorderButton.textContent = "STOP";
+        recording = true;
+    } else {
+        recorder.stop();
+    }
+});
